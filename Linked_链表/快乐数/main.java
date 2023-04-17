d4623972d58c288b7a57032643710eef299eb9db
Zhashut <2105798885@qq.com>
2023-04-17 20:22:41 +0800

    add LC-202. 快乐数

---
+++ Linked_链表/快乐数/main.java
@@ -0,0 +1,35 @@
+package Linked_链表.快乐数;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: 炸薯条
+ * Date: 2023/4/17
+ * Time: 20:08
+ * Description: No Description
+ */
+public class main {
+    public boolean isHappy(int n) {
+        Set<Integer> record = new HashSet<>();
+        while (n != 1 && !record.contains(n)) {
+            record.add(n);
+            n = getNextNums(n);
+        }
+
+        return n == 1;
+    }
+
+    private int getNextNums(int n) {
+        int res = 0;
+        while (n > 0) {
+            int temp = n % 10;
+            res += temp * temp;
+            n /= 10;
+        }
+
+        return res;
+    }
+}
