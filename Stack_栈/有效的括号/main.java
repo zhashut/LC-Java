7d754075011e9895482a4d7dd7cb7292baa7ecfd
Zhashut <2105798885@qq.com>
2023-04-24 19:14:38 +0800

    add LC-20. 有效的括号

---
+++ Stack_栈/有效的括号/main.java
@@ -0,0 +1,37 @@
+package Stack_栈.有效的括号;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: 炸薯条
+ * Date: 2023/4/24
+ * Time: 18:52
+ * Description: https://leetcode.cn/problems/valid-parentheses/
+ */
+public class main {
+
+    public boolean isValid(String s) {
+        Deque<Character> deque = new LinkedList<>();
+        char ch;
+        for (int i = 0; i < s.length(); i++) {
+            ch = s.charAt(i);
+            //碰到左括号，就把相应的右括号入栈
+            if (ch == '(') {
+                deque.push(')');
+            } else if (ch == '{') {
+                deque.push('}');
+            } else if (ch == '[') {
+                deque.push(']');
+            } else if (deque.isEmpty() || deque.peek() != ch) {
+                return false;
+            } else {//如果是右括号判断是否和栈顶元素匹配
+                deque.pop();
+            }
+        }
+        //最后判断栈中元素是否匹配
+        return deque.isEmpty();
+    }
+}
