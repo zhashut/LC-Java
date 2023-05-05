346a90fcc2904957eb977ed85f9ddf3a5fe70e25
Zhashut <2105798885@qq.com>
2023-05-05 21:43:31 +0800

    add LC-235. 二叉搜索树的最近公共祖先

---
+++ BinaryTree_二叉树/二叉搜索树的最近公共祖先/main.java
@@ -0,0 +1,53 @@
+package BinaryTree_二叉树.二叉搜索树的最近公共祖先;
+
+import BinaryTree_二叉树.TreeNode;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: 炸薯条
+ * Date: 2023/5/5
+ * Time: 21:00
+ * Description: https://leetcode.cn/problems/lowest-common-ancestor-of-a-binary-search-tree/
+ */
+public class main {
+    /**
+     * 递归法
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root.val > p.val && root.val > q.val) {
+            TreeNode left = lowestCommonAncestor(root.left, p, q);
+            if (left != null) {
+                return left;
+            }
+        }
+        if (root.val < p.val && root.val < q.val) {
+            TreeNode right = lowestCommonAncestor(root.right, p, q);
+            if (right != null) {
+                return right;
+            }
+        }
+
+        return root;
+    }
+
+    /**
+     * 迭代法
+     */
+    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+        while (true) {
+            if (root.val > p.val && root.val > q.val) {
+                root = root.left;
+            } else if (root.val < p.val && root.val < q.val) {
+                root = root.right;
+            } else {
+                break;
+            }
+        }
+        return root;
+    }
+}
