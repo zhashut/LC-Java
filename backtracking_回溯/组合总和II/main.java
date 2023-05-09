2ea08ae6b33af9b8d282a5c2f059097bf7c75ca8
Zhashut <2105798885@qq.com>
2023-05-09 20:22:36 +0800

    add LC-40. 组合总和 II

--- backtracking_回溯/组合总和II/main.java
+++ backtracking_回溯/组合总和II/main.java
@@ -1,6 +1,7 @@
 package backtracking_回溯.组合总和II;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -15,10 +16,17 @@ import java.util.List;
 public class main {
     List<List<Integer>> res;
     LinkedList<Integer> path;
+    boolean[] used;
 
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         res = new ArrayList<>();
         path = new LinkedList<>();
+        used = new boolean[candidates.length];
+        // 加标志数组，用来辅助判断同层节点是否已经遍历
+        Arrays.fill(used, false);
+        // 为了将重复的数字都放到一起，所以先进行排序
+        Arrays.sort(candidates);
+
         backTracing(candidates, target, 0, 0);
         return res;
     }
@@ -32,8 +40,13 @@ public class main {
             if (sum + candidates[i] > target) {
                 break;
             }
+            if (i > 0 && candidates[i] == candidates[i - 1] && !used[i - 1]) {
+                continue;
+            }
             path.add(candidates[i]);
+            used[i] = true;
             backTracing(candidates, target, sum + candidates[i], i + 1);
+            used[i] = false;
             path.removeLast();
         }
     }
