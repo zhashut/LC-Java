b6b9780bdf76dedf2700ea88e9c6d1bf6a5e7263
Zhashut <2105798885@qq.com>
2023-05-21 20:53:59 +0800

    add LC-56. 合并区间

---
+++ Greedy_贪心/合并区间/main.java
@@ -0,0 +1,57 @@
+package Greedy_贪心.合并区间;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: 炸薯条
+ * Date: 2023/5/21
+ * Time: 6:48
+ * Description: https://leetcode.cn/problems/merge-intervals/
+ */
+public class main {
+    // 解法一
+    public int[][] merge(int[][] intervals) {
+        List<int[]> res = new LinkedList<>();
+        //按照左边界排序
+        Arrays.sort(intervals, (x, y) -> Integer.compare(x[0], y[0]));
+        //initial start 是最小左边界
+        int start = intervals[0][0];
+        int rightmostRightBound = intervals[0][1];
+        for (int i = 1; i < intervals.length; i++) {
+            //如果左边界大于最大右边界
+            if (intervals[i][0] > rightmostRightBound) {
+                //加入区间 并且更新start
+                res.add(new int[]{start, rightmostRightBound});
+                start = intervals[i][0];
+                rightmostRightBound = intervals[i][1];
+            } else {
+                //更新最大右边界
+                rightmostRightBound = Math.max(rightmostRightBound, intervals[i][1]);
+            }
+        }
+        res.add(new int[]{start, rightmostRightBound});
+        return res.toArray(new int[res.size()][]);
+    }
+
+    // 解法二
+    public int[][] merge2(int[][] intervals) {
+        LinkedList<int[]> res = new LinkedList<>();
+        Arrays.sort(intervals, (o1, o2) -> Integer.compare(o1[0], o2[0]));
+        res.add(intervals[0]);
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] <= res.getLast()[1]) {
+                int start = res.getLast()[0];
+                int end = Math.max(intervals[i][1], res.getLast()[1]);
+                res.removeLast();
+                res.add(new int[]{start, end});
+            } else {
+                res.add(intervals[i]);
+            }
+        }
+        return res.toArray(new int[res.size()][]);
+    }
+}
