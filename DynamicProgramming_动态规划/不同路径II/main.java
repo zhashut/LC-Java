9b079fe9b1e926bcaa711797575b62a7aa48fabb
Zhashut <2105798885@qq.com>
2023-05-25 06:34:04 +0800

    add LC-63. 不同路径 II

---
+++ DynamicProgramming_动态规划/不同路径II/main.java
@@ -0,0 +1,33 @@
+package DynamicProgramming_动态规划.不同路径II;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: 炸薯条
+ * Date: 2023/5/25
+ * Time: 6:14
+ * Description: https://leetcode.cn/problems/unique-paths-ii/
+ */
+public class main {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length, n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m && obstacleGrid[i][0] == 0; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 0; i < n && obstacleGrid[0][i] == 0; i++) {
+            dp[0][i] = 1;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    continue;
+                }
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+
+        return dp[m-1][n-1];
+    }
+}
