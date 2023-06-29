ee4d658bde6488906996e60e126f87d8501705d5
Zhashut <2105798885@qq.com>
2023-06-29 11:12:05 +0800

    add LC-392. 判断子序列

---
+++ DynamicProgramming_动态规划/判断子序列/main.java
@@ -0,0 +1,58 @@
+package DynamicProgramming_动态规划.判断子序列;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: 炸薯条
+ * Date: 2023/6/29
+ * Time: 10:47
+ * Description: https://leetcode.cn/problems/is-subsequence/
+ */
+public class main {
+    public boolean isSubsequence(String s, String t) {
+        int len1 = s.length(), len2 = t.length();
+        int[][] dp = new int[len1 + 1][len2 + 1];
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= len2; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = dp[i][j - 1];
+                }
+            }
+        }
+
+        if (dp[len1][len2] == len1) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean isSubsequence2(String s, String t) {
+        int n = s.length(), m = t.length();
+
+        int[][] f = new int[m + 1][26];
+        for (int i = 0; i < 26; i++) {
+            f[m][i] = m;
+        }
+
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = 0; j < 26; j++) {
+                if (t.charAt(i) == j + 'a')
+                    f[i][j] = i;
+                else
+                    f[i][j] = f[i + 1][j];
+            }
+        }
+        int add = 0;
+        for (int i = 0; i < n; i++) {
+            if (f[add][s.charAt(i) - 'a'] == m) {
+                return false;
+            }
+            add = f[add][s.charAt(i) - 'a'] + 1;
+        }
+        return true;
+    }
+
+}
